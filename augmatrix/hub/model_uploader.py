f04ce7af179ca64e0570589ecfa167044b657b4f
Sunilkumar <sunilkumar@augmatrix.io>
2023-08-02 19:50:46 +0530

    update the metrics issus and add constants to hub

--- augmatrix/hub/model_uploader.py
+++ augmatrix/hub/model_uploader.py
@@ -1,65 +1,112 @@
 import os
+import json
 from pathlib import Path
 import urllib.parse
 import requests
 from tqdm import tqdm
+from constants.model_constants import SINGLE_LINE_FILES
 
 
 class AugmatrixUploader:
     def __init__(self, file_path):
         self.token = os.environ.get('AUGMATRIX_TOKEN')
         self.base_url = os.environ.get('AUGMATRIX_API_URL')
-        self.experiment_name = os.environ.get('MLFLOW_EXPERIMENT_NAME')
-        self.model_name = os.environ.get('SELECTED_MODEL_NAME')
-        self.file_path = file_path
+        self.experiment_name = os.environ.get('EXPERIMENT_NAME')
+        self.model_name = os.environ.get('MODEL_NAME')
+        self.pipeline_tag = os.environ.get('PIPELINE_NAME')
 
         if self.token is None:
             raise ValueError("AGUMATRIX_TOKEN environment variable not set.")
 
         if self.experiment_name is None:
-            raise ValueError("environmental variable MLFLOW_EXPERIMENT_NAME not set")
+            raise ValueError("experiment_name is not set")
 
         if self.model_name is None:
-            raise ValueError("environmental variable SELECTED_MODEL_NAME not set")
+            raise ValueError("model is not set")
 
         self.upload_url = urllib.parse.urljoin(self.base_url, "api/auth_upload_model_info/")
+        self.file_path = file_path
 
-    def binary_data_generator(self, file_path):
-        with open(file_path, "rb") as file:
-            chunk_size = 8192
-            while True:
-                data_chunk = file.read(chunk_size)
-                if len(data_chunk) == 0:
-                    break
-                yield ("file", (file_path, data_chunk))
-
-    def push_to_metrics(self, chunk_size=8192):
+    def converting_the_metrics_to_json(self, file_path):
+        labels = []
+        datasets = []
+        epoch_set = set()
+        sending_data_json = {}
         try:
-            for root, _, files in os.walk(self.file_path):
+            for root, _, files in os.walk(file_path):
                 for filename in files:
-                    print(filename)
-                    file_path = os.path.join(root, filename)
-                    headers = {
-                        "Authorization": f"Token {self.token}",
-                    }
-
-                    data = {"experiment_name": self.experiment_name,"file_type":"matrices", "model_name": self.model_name}
-
-                    response = requests.post(
-                        self.upload_url,
-                        files=self.binary_data_generator(file_path),
-                        headers=headers,
-                        data=data, 
-                        stream=True,
-                    )
-
-                    if response.status_code == 200:
-                        print("Chunk sent successfully!")
-                    else:
-                        print("Error sending chunk. Status code:", response.status_code)
+                    if str(filename) not in SINGLE_LINE_FILES:
+                        label_data = []
+                        file_path = os.path.join(root, filename)
+                        
+                        with open(file_path, "r") as file:
+                            chunk_size = 8192
+                            while True:
+                                data_chunk = file.read(chunk_size)
+                                if len(data_chunk) == 0:
+                                    break
+                                lines = data_chunk.strip().split("\n")
+                                for line in lines:
+                                    parts = line.split()
+                                    if len(parts) >= 3:
+                                        # time = parts[0]
+                                        value = parts[1]
+                                        epoch = parts[-1]
+                                        
+        
+                                        try:
+                                            epoch = int(epoch)
+                                            value = int(value)
+                                        except ValueError:
+                                            epoch = float(epoch)
+                                            value = float(value)
+    
+                                        if filename != "epoch": 
+                                            label_data.append(value)
+        
+                                        elif filename == "epoch" and value not in epoch_set:
+                                            labels.append(value)
+                                            epoch_set.add(value)
+                                if filename != "epoch": 
+                                    dataset = {
+                                        "label": filename,
+                                        "data": label_data,
+                                    }
+                                    datasets.append(dataset)
+                                sending_data_json = {"label": list(labels), "dataset": datasets}
+            return sending_data_json
         except FileNotFoundError:
             print("Directory not found:", self.file_path)
- 
+
+    def push_to_metrics(self):
+        try:
+            sending_data_json = self.converting_the_metrics_to_json(self.file_path)
+            sending_binary_data = json.dumps(sending_data_json, ensure_ascii=False).encode('utf-8')
+            headers = {"Authorization": f"Token {self.token}"}
+            
+            data = {
+                        "experiment_name": self.experiment_name,
+                        "file_type": "metrice",
+                        "model_name": self.model_name,
+                        "model_metrices" : sending_binary_data,
+                        "pipeline_tag" : self.pipeline_tag
+                    }
+                            
+            response = requests.post(
+                self.upload_url,
+                headers=headers,
+                data=data,
+                stream=True,
+            )
+    
+            if response.status_code != 200:
+                print("Error sending chunk. Status code:", response.status_code)
+                
+            print("Merics uploaded successfully")
+            
+        except UnicodeDecodeError as e:
+            print("Directory not found:", e)
+    
     def artifacts_binary_data_generator(self, file_path):
         with open(file_path, 'rb') as file:
             chunk_size = 2000 * 1024 * 1024
@@ -72,25 +119,34 @@ class AugmatrixUploader:
     def push_to_hub(self):
         try:
             if os.path.isdir(self.file_path):
+                total_uploaded_size = 0
+                total_file_size = 0
+                total_chunks = 0
+                chunk_number = 1
+                
                 for root, _, files in os.walk(self.file_path):
-                    chunk_number = 1
                     for filename in files:
                         file_path = os.path.join(root, filename)
+                        total_file_size += os.path.getsize(file_path)
+                        total_chunks += 1
 
-                        headers = {
-                            "Authorization": f"Token {self.token}",
-                        }
-                        data = {
-                            "experiment_name": self.experiment_name,
-                            "file_type": "output",
-                            "model_name": self.model_name,
-                            "chunk_number": chunk_number,
-                        }
-                        total_file_size = os.path.getsize(file_path)
-                        chunk_size = 500 * 1024 * 1024
-                        num_iterations = (total_file_size + chunk_size - 1) // chunk_size
+                headers = {
+                    "Authorization": f"Token {self.token}",
+                }
+                data = {
+                    "experiment_name": self.experiment_name,
+                    "file_type": "output",
+                    "model_name": self.model_name,
+                    "pipeline_tag" : self.pipeline_tag,
+                    "chunk_number" : chunk_number
+                }
+                chunk_size = 5 * 1024 * 1024
+                num_iterations = (total_file_size + chunk_size - 1) // chunk_size
+                with tqdm(total=total_file_size, desc="Uploading directory", unit="B") as pbar:
+                    for root, _, files in os.walk(self.file_path):
+                        for filename in files:
+                            file_path = os.path.join(root, filename)
 
-                        with tqdm(total=num_iterations, desc=f"Uploading {filename}", unit="chunk") as pbar:
                             with open(file_path, "rb") as f:
                                 while True:
                                     chunk = f.read(chunk_size)
@@ -103,13 +159,14 @@ class AugmatrixUploader:
                                         data=data,
                                         stream=True,
                                     )
-                                    chunk_number += 1
-                                    pbar.update(1)
+                                    total_uploaded_size += len(chunk)
+                                    pbar.update(len(chunk))
+                                    chunk_number = chunk_number + 1
 
-                                    if response.status_code == 200:
-                                        print("Chunk sent successfully!")
-                                    else:
+                                    if response.status_code != 200:
                                         print("Error sending chunk. Status code:", response.status_code)
+
+                print(f"Total uploaded size: {total_uploaded_size}/{total_file_size} bytes")
 
         except FileNotFoundError:
             print("Directory not found:", self.file_path)
