9fcc684e5f9c4e525310ae858c1b2278db0c3973
Sunilkumar <sunilkumar@augmatrix.io>
2023-08-03 13:26:30 +0530

    updated the hub code to work with backend api

--- augmatrix/hub/model_uploader.py
+++ augmatrix/hub/model_uploader.py
@@ -9,11 +9,11 @@ from constants.model_constants import SINGLE_LINE_FILES
 
 class AugmatrixUploader:
     def __init__(self, file_path):
-        self.token = os.environ.get('AUGMATRIX_TOKEN')
-        self.base_url = os.environ.get('AUGMATRIX_API_URL')
-        self.experiment_name = os.environ.get('EXPERIMENT_NAME')
-        self.model_name = os.environ.get('MODEL_NAME')
-        self.pipeline_tag = os.environ.get('PIPELINE_NAME')
+        self.token = os.environ.get("AUGMATRIX_TOKEN")
+        self.base_url = os.environ.get("AUGMATRIX_API_URL")
+        self.experiment_name = os.environ.get("EXPERIMENT_NAME")
+        self.model_name = os.environ.get("SELECTED_MODEL_NAME")
+        self.pipeline_tag = os.environ.get("PIPELINE_NAME")
 
         if self.token is None:
             raise ValueError("AGUMATRIX_TOKEN environment variable not set.")
@@ -24,7 +24,12 @@ class AugmatrixUploader:
         if self.model_name is None:
             raise ValueError("model is not set")
 
-        self.upload_url = urllib.parse.urljoin(self.base_url, "api/auth_upload_model_info/")
+        self.upload_url = urllib.parse.urljoin(
+            self.base_url, "api/auth_upload_model_info/"
+        )
+        self.model_metrics_url = urllib.parse.urljoin(
+            self.base_url, "api/auth_upload_model_metrics/"
+        )
         self.file_path = file_path
 
     def converting_the_metrics_to_json(self, file_path):
@@ -38,7 +43,7 @@ class AugmatrixUploader:
                     if str(filename) not in SINGLE_LINE_FILES:
                         label_data = []
                         file_path = os.path.join(root, filename)
-                        
+
                         with open(file_path, "r") as file:
                             chunk_size = 8192
                             while True:
@@ -52,28 +57,33 @@ class AugmatrixUploader:
                                         # time = parts[0]
                                         value = parts[1]
                                         epoch = parts[-1]
-                                        
-        
+
                                         try:
                                             epoch = int(epoch)
                                             value = int(value)
                                         except ValueError:
                                             epoch = float(epoch)
                                             value = float(value)
-    
-                                        if filename != "epoch": 
+
+                                        if filename != "epoch":
                                             label_data.append(value)
-        
-                                        elif filename == "epoch" and value not in epoch_set:
+
+                                        elif (
+                                            filename == "epoch"
+                                            and value not in epoch_set
+                                        ):
                                             labels.append(value)
                                             epoch_set.add(value)
-                                if filename != "epoch": 
+                                if filename != "epoch":
                                     dataset = {
                                         "label": filename,
                                         "data": label_data,
                                     }
                                     datasets.append(dataset)
-                                sending_data_json = {"label": list(labels), "dataset": datasets}
+                                sending_data_json = {
+                                    "label": list(labels),
+                                    "dataset": datasets,
+                                }
             return sending_data_json
         except FileNotFoundError:
             print("Directory not found:", self.file_path)
@@ -81,34 +91,35 @@ class AugmatrixUploader:
     def push_to_metrics(self):
         try:
             sending_data_json = self.converting_the_metrics_to_json(self.file_path)
-            sending_binary_data = json.dumps(sending_data_json, ensure_ascii=False).encode('utf-8')
+            sending_binary_data = json.dumps(
+                sending_data_json, ensure_ascii=False
+            ).encode("utf-8")
             headers = {"Authorization": f"Token {self.token}"}
-            
+
             data = {
-                        "experiment_name": self.experiment_name,
-                        "file_type": "metrice",
-                        "model_name": self.model_name,
-                        "model_metrices" : sending_binary_data,
-                        "pipeline_tag" : self.pipeline_tag
-                    }
-                            
+                "experiment_name": self.experiment_name,
+                "selected_model_name": self.model_name,
+                "model_metrics": sending_binary_data,
+                "pipeline_tag": self.pipeline_tag,
+            }
+
             response = requests.post(
-                self.upload_url,
+                self.model_metrics_url,
                 headers=headers,
                 data=data,
                 stream=True,
             )
-    
+
             if response.status_code != 200:
                 print("Error sending chunk. Status code:", response.status_code)
-                
+
             print("Merics uploaded successfully")
-            
+
         except UnicodeDecodeError as e:
             print("Directory not found:", e)
-    
+
     def artifacts_binary_data_generator(self, file_path):
-        with open(file_path, 'rb') as file:
+        with open(file_path, "rb") as file:
             chunk_size = 2000 * 1024 * 1024
             while True:
                 data_chunk = file.read(chunk_size)
@@ -123,7 +134,7 @@ class AugmatrixUploader:
                 total_file_size = 0
                 total_chunks = 0
                 chunk_number = 1
-                
+
                 for root, _, files in os.walk(self.file_path):
                     for filename in files:
                         file_path = os.path.join(root, filename)
@@ -135,14 +146,15 @@ class AugmatrixUploader:
                 }
                 data = {
                     "experiment_name": self.experiment_name,
-                    "file_type": "output",
-                    "model_name": self.model_name,
-                    "pipeline_tag" : self.pipeline_tag,
-                    "chunk_number" : chunk_number
+                    "selected_model_name": self.model_name,
+                    "pipeline_tag": self.pipeline_tag,
+                    "chunk_number": chunk_number,
                 }
                 chunk_size = 5 * 1024 * 1024
                 num_iterations = (total_file_size + chunk_size - 1) // chunk_size
-                with tqdm(total=total_file_size, desc="Uploading directory", unit="B") as pbar:
+                with tqdm(
+                    total=total_file_size, desc="Uploading directory", unit="B"
+                ) as pbar:
                     for root, _, files in os.walk(self.file_path):
                         for filename in files:
                             file_path = os.path.join(root, filename)
@@ -154,7 +166,9 @@ class AugmatrixUploader:
                                         break
                                     response = requests.post(
                                         self.upload_url,
-                                        files=self.artifacts_binary_data_generator(file_path),
+                                        files=self.artifacts_binary_data_generator(
+                                            file_path
+                                        ),
                                         headers=headers,
                                         data=data,
                                         stream=True,
@@ -164,9 +178,14 @@ class AugmatrixUploader:
                                     chunk_number = chunk_number + 1
 
                                     if response.status_code != 200:
-                                        print("Error sending chunk. Status code:", response.status_code)
+                                        print(
+                                            "Error sending chunk. Status code:",
+                                            response.status_code,
+                                        )
 
-                print(f"Total uploaded size: {total_uploaded_size}/{total_file_size} bytes")
+                print(
+                    f"Total uploaded size: {total_uploaded_size}/{total_file_size} bytes"
+                )
 
         except FileNotFoundError:
             print("Directory not found:", self.file_path)
